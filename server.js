995681e2804d702159db235068398f7db850e73b
Jade Meskill <jade.meskill@gmail.com>
2014-10-01 21:35:55 -0700

    cleanup

--- server.js
+++ server.js
@@ -11,25 +11,23 @@ config         = require('./config/config'),
 skynet         = require('skynet'),
 errorHandler   = require('errorhandler');
 
-var port       = process.env.EMAIL_PORT || process.env.PORT || 9011;
 var meshblu    = skynet.createConnection(config.meshblu);
 var meshbluAuth = require('./middleware/meshblu-auth');
 
 app = express();
-app.use(bodyParser.json());
+app.set('port', process.env.EMAIL_PORT || process.env.PORT || 9011);
 app.use(session({resave: true, saveUninitialized: true, secret: 'sqrt0fSaturn'}));
 app.use(morgan('combined'));
 app.use(methodOverride());
-app.use(errorHandler());
+app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(errorHandler());
 
 var MessageController = require('./controllers/message-controller');
 var messageController = new MessageController();
 
 app.post('/messages', meshbluAuth.authenticate, messageController.create);
 
-var httpServer = http.createServer(app);
-
-httpServer.listen(port, function(){
-  console.log('Listening on port ' + port);
+app.listen(app.get('port'), function() {
+  console.log("Express server listening on port " + app.get('port'));
 });
