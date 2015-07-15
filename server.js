b6af8def87d04d95622cf38a6b69c7917ff9b3a7
Peter DeMartini <thepeterdemartini@gmail.com>
2015-07-15 16:10:04 -0700

    Rate limit emails

--- server.js
+++ server.js
@@ -1,33 +1,43 @@
-var express = require('express'),
-http        = require('http'),
-https        = require('https'),
-path        = require('path'),
-fs          = require('fs'),
-morgan        = require('morgan'),
+var express    = require('express'),
+http           = require('http'),
+https          = require('https'),
+path           = require('path'),
+fs             = require('fs'),
+morgan         = require('morgan'),
 methodOverride = require('method-override'),
 session        = require('express-session'),
 bodyParser     = require('body-parser'),
 config         = require('./config/config'),
-meshblu        = require('meshblu'),
 errorHandler   = require('errorhandler');
 _              = require('lodash');
-
-var meshblu    = meshblu.createConnection(_.clone(config.meshblu));
-var meshbluAuth = require('./middleware/meshblu-auth');
 
 app = express();
 app.set('port', process.env.EMAIL_PORT || process.env.PORT || 9011);
 app.use(session({resave: true, saveUninitialized: true, secret: 'sqrt0fSaturn'}));
-app.use(morgan('combined'));
+app.use(morgan('dev'));
 app.use(methodOverride());
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(errorHandler());
 
+var meshbluAuth = require('express-meshblu-auth');
+app.use(meshbluAuth({
+  server: 'meshblu.octoblu.com',
+  port: 443,
+  protocol: 'https'
+}));
+
+var meshbluRatelimit = require('express-meshblu-ratelimit');
+app.use(meshbluRatelimit());
+
+var meshbluHealthcheck = require('express-meshblu-healthcheck');
+app.use(meshbluHealthcheck());
+
+
 var MessageController = require('./controllers/message-controller');
 var messageController = new MessageController();
 
-app.post('/messages', meshbluAuth.authenticate, messageController.create);
+app.post('/messages', messageController.create);
 
 app.listen(app.get('port'), function() {
   console.log("Express server listening on port " + app.get('port'));
