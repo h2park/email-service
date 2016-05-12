08696ad358b7676d0929e5eacfebc59990212bdb
Jade Meskill <jade.meskill@gmail.com>
2016-05-12 10:59:27 -0700

    v1.1.0 use latest express-meshblu-auth

--- server.js
+++ server.js
@@ -1,40 +1,42 @@
-var express    = require('express'),
-http           = require('http'),
-https          = require('https'),
-path           = require('path'),
-fs             = require('fs'),
-morgan         = require('morgan'),
-methodOverride = require('method-override'),
-session        = require('express-session'),
-bodyParser     = require('body-parser'),
-config         = require('./config/config'),
-errorHandler   = require('errorhandler');
-_              = require('lodash');
-
+var express            = require('express');
+var http               = require('http');
+var https              = require('https');
+var path               = require('path');
+var fs                 = require('fs');
+var morgan             = require('morgan');
+var methodOverride     = require('method-override');
+var session            = require('express-session');
+var bodyParser         = require('body-parser');
+var config             = require('./config/config');
+var errorHandler       = require('errorhandler');
+var _                  = require('lodash');
+var MeshbluAuth        = require('express-meshblu-auth');
+var expressVersion     = require('express-package-version');
 var meshbluHealthcheck = require('express-meshblu-healthcheck');
+var meshbluRatelimit   = require('express-meshblu-ratelimit');
+var MessageController  = require('./controllers/message-controller');
+
+var meshbluAuth = new MeshbluAuth({
+  server: 'meshblu.octoblu.com',
+  port: 443,
+  protocol: 'https'
+});
+
+var messageController = new MessageController();
 
 app = express();
 app.set('port', process.env.EMAIL_PORT || process.env.PORT || 80);
 app.use(meshbluHealthcheck());
-app.use(session({resave: true, saveUninitialized: true, secret: 'sqrt0fSaturn'}));
+app.use(expressVersion({format: '{"version": "%s"}'}));
 app.use(morgan('dev', { skip: function(req, res){ return res.statusCode < 400 }}));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(methodOverride());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 app.use(errorHandler());
-
-var meshbluAuth = require('express-meshblu-auth');
-app.use(meshbluAuth({
-  server: 'meshblu.octoblu.com',
-  port: 443,
-  protocol: 'https'
-}));
-
-var meshbluRatelimit = require('express-meshblu-ratelimit');
+app.use(meshbluAuth.retrieve());
+app.use(meshbluAuth.gateway());
+app.use(session({resave: true, saveUninitialized: true, secret: 'sqrt0fSaturn'}));
 app.use(meshbluRatelimit());
-
-var MessageController = require('./controllers/message-controller');
-var messageController = new MessageController();
 
 app.post('/messages', messageController.create);
 
